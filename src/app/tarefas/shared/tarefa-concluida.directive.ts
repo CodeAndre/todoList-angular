74bb3f1930433b5164df3db983df79fcdba3ab75
Andre Viana <code.andre3@gmail.com>
2021-07-08 08:00:14 -0300

    final commit :tada:

--- src/app/tarefas/shared/tarefa-concluida.directive.ts
+++ src/app/tarefas/shared/tarefa-concluida.directive.ts
@@ -1,10 +1,20 @@
-import { Directive } from '@angular/core';
+import { 
+  Directive, ElementRef, Input, OnInit 
+} from '@angular/core';
 
 @Directive({
-  selector: '[appTarefaConcluida]'
+  selector: '[tarefaConcluida]'
 })
-export class TarefaConcluidaDirective {
+export class TarefaConcluidaDirective implements OnInit {
 
-  constructor() { }
+  @Input() tarefaConcluida: boolean; //Entrada de dados, do tipo boolean para passar os dados
+
+  constructor(private el: ElementRef) { } //ElementRef --> realizar a referencia do HTML </ td>
+
+  ngOnInit(): void {
+    if (this.tarefaConcluida) { //se estiver concluida
+      this.el.nativeElement.style.textDecoration = "line-through"; //pego o elemento, chamo o nativeElement(td) e defino um estilo
+    }
+  }
 
 }
